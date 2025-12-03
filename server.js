6fe5c01834bd9f5e4e356e1259ec59b95f9ee291
Morkosabanoub <163788462+Morkosabanoub@users.noreply.github.com>
2025-12-04 00:34:36 +0200

    .

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(
 
 app.use(express.json());
 
-const client = new MongoClient(process.env.MONGO_URI);
+const client = new MongoClient(process.env.mongouri);
 
 let translationsCollection;
 
